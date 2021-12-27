2b83525160aea3ff97417e0f52a1d5f9cfdf92fa
eypbatihan <87443672+eypbatihan@users.noreply.github.com>
2021-12-27 23:41:34 +0300

    metu

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Button from "@mui/material/Button";
 import email from "../assests/email.svg";
 import phone from "../assests/phone.svg";
 import location from "../assests/location.svg";
